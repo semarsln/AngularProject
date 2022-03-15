5854b8fc338710a8cf67359bb28b0df4681d4c37
sema arslan <semaarslan1999@gmail.com>
2022-03-15 13:09:37 +0300

    changing

--- proje/angular/src/app/tests/alltest/alltest.component.ts
+++ proje/angular/src/app/tests/alltest/alltest.component.ts
@@ -5,12 +5,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './alltest.component.html',
   styleUrls: ['./alltest.component.scss']
 })
+
+
 export class AlltestComponent{
-  users = [
-    { no: '1', category: 'frank.murphy@test.com', question: 'User' },
-    { no: '2', category: 'vic.reynolds@test.com', question: 'Admin' },
-    { no: '3',  category: 'gina.jabowski@test.com', question: 'Admin' },
-    { no: '4', category: 'jessi.glaser@test.com', question: 'User' },
-    { no: '5',  category: 'jay.bilzerian@test.com', question: 'User' }
-];
+  editField: string;
+ 
+
+  personList: Array<any> = [
+    { id: 1, category: 'c', question: "Specify different types of decision control statements?", },
+    { id: 2, category: 'python', question: "What is a dynamically typed language?", },
+    { id: 3, category: 'php', question: "What is the difference between static and dynamic websites?", },
+    { id: 4, category: 'java', question: "What do you understand by an instance variable and a local variable?",  },
+    { id: 5, category: 'Angular', question: "Why were client-side frameworks like Angular introduced?",  },
+  ];
+
+  awaitingPersonList: Array<any> = [
+    { id: 6, category: 'George Vega', question: 28 },
+    { id: 7, category: 'Mike Low', question: 22 },
+    { id: 8, category: 'John Derp', question: 36 },
+    { id: 9, category: 'Anastasia John', question: 21 },
+    { id: 10, category: 'John Maklowicz', question: 36},
+  ];
+
+  updateList(id: number, property: string, event: any) {
+    const editField = event.target.textContent;
+    this.personList[id][property] = editField;
+  }
+
+
+
+  add() {
+    if (this.awaitingPersonList.length > 0) {
+      const person = this.awaitingPersonList[0];
+      this.personList.push(person);
+      this.awaitingPersonList.splice(0, 1);
+    }
+  }
+
+  changeValue(id: number, property: string, event: any) {
+    this.editField = event.target.textContent;
+  }
 }
